aadb23764099d2ff4a13a1651ee4109b4cc000ee
Shailendra Chauhan <Shailendra.Chauhan@IN.exelaonline.com>
2021-07-24 11:19:27 +0530

    initial commit

---
+++ src/main/java/com/shailu/dockerjenkinspocc/DockerjenkinspoccApplication.java
@@ -0,0 +1,13 @@
+package com.shailu.dockerjenkinspocc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerjenkinspoccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerjenkinspoccApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shailu/dockerjenkinspocc/DockerjenkinspoccApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shailu.dockerjenkinspocc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerjenkinspoccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
